751aa54e00ff8d3c6ac8e6ae5408168f4140a3c6
tongdayong <bryanxtong@gmail.com>
2024-03-28 14:58:39 +0800

    remove customized code and use spring-boot-admin-server-cloud instead

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #Showcase - Spring Boot Admin server
 HTTP Based DiscoveryClient
 It depends on spring cloud kubernetes discovery server version which needs to be deployed into the k8s cluster
+
+Remove all the customized code for spring boot admin server and use spring-boot-admin-server-cloud instead
--- src/main/java/org/s1p/demo/spring/boot/admin/config/SecurityConfiguration.java
+++ src/main/java/org/s1p/demo/spring/boot/admin/config/SecurityConfiguration.java
@@ -15,21 +15,8 @@
  */
 
 package org.s1p.demo.spring.boot.admin.config;
-import org.s1p.demo.spring.boot.admin.discovery.InstanceDiscoveryListener;
-import org.s1p.demo.spring.boot.admin.discovery.KubernetesServiceInstanceConverter;
-import org.s1p.demo.spring.boot.admin.discovery.ServiceInstanceConverter;
-import de.codecentric.boot.admin.server.config.AdminServerAutoConfiguration;
-import de.codecentric.boot.admin.server.config.AdminServerMarkerConfiguration;
+
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
-import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
-import de.codecentric.boot.admin.server.services.InstanceRegistry;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -40,37 +27,7 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
-//@ConditionalOnSingleCandidate(DiscoveryClient.class)
-//@ConditionalOnBean(AdminServerMarkerConfiguration.Marker.class)
-@ConditionalOnProperty(prefix = "spring.boot.admin.discovery", name = "enabled", matchIfMissing = true)
-@AutoConfigureAfter(value = AdminServerAutoConfiguration.class, name = {
-        "org.springframework.cloud.kubernetes.discovery.KubernetesDiscoveryClientAutoConfiguration",
-        "org.springframework.cloud.client.discovery.simple.SimpleDiscoveryClientAutoConfiguration"})
-public class AdminServerDiscoveryAutoConfiguration {
-
-    @Bean
-    @ConditionalOnMissingBean
-    @ConfigurationProperties(prefix = "spring.boot.admin.discovery")
-    public InstanceDiscoveryListener instanceDiscoveryListener(ServiceInstanceConverter serviceInstanceConverter,
-                                                               DiscoveryClient discoveryClient,
-                                                               InstanceRegistry registry,
-                                                               InstanceRepository repository) {
-        InstanceDiscoveryListener listener = new InstanceDiscoveryListener(discoveryClient, registry, repository);
-        listener.setConverter(serviceInstanceConverter);
-        return listener;
-    }
-
-    @Configuration
-    @ConditionalOnMissingBean({ServiceInstanceConverter.class})
-    //@ConditionalOnBean(KubernetesClient.class)
-    public static class KubernetesConverterConfiguration {
-        @Bean
-        @ConfigurationProperties(prefix = "spring.boot.admin.discovery.converter")
-        public KubernetesServiceInstanceConverter serviceInstanceConverter() {
-            return new KubernetesServiceInstanceConverter();
-        }
-    }
-
+public class SecurityConfiguration {
     @Profile("secure")
     @Configuration
     public static class SecuritySecureConfig {
